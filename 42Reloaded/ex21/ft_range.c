644df5217c7c1ee578b815fe57a26dd151a38929
K4p1o <121300541+DiniMB@users.noreply.github.com>
2025-10-15 19:25:56 +0100

    Add files via upload

---
+++ 42Reloaded/ex21/ft_range.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_range.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dbaltaza <dbaltaza@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/24 12:35:42 by dbaltaza          #+#    #+#             */
+/*   Updated: 2025/09/24 13:07:34 by dbaltaza         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int *ft_range(int min, int max)
+{
+	int i;
+	int *array;
+
+	if (min >= max)
+		return (NULL);
+
+	array = malloc(sizeof(int) * (max - min));
+	if (array == NULL)
+		return (NULL);
+	i = 0;
+	while (min < max)
+	{
+		array[i] = min;
+		min++;
+		i++;
+	}
+	return (array);
+}
+
